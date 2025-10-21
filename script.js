3f57a9169c328f5506fad53215e145bfa4d9d628
o00o-11 <gluck_94@naver.com>
2025-10-21 10:07:32 +0900

    03

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
 const handleSearch = () => {
-    alert("Hello World");
+    const input = document.getElementById("searchInput").value.trim();
+    console.log(input);
+
+    if (input !== "") {
+        gtag("event", "search_button_clicked", {
+            event_category: "engagement",
+            event_label: "search_keyword",
+            search_term: input
+        });
+
+        alert(`검색어 ${input}에 대한 검색을 실행하였습니다.`);
+    } else {
+        alert("검색어를 입력해주세요.");
+    }
 };
